a1d30c188455ea5e3c0c65980f966861f2a9f23a
LuisAndradeDias <luisphelipeandrade@gmail.com>
2022-02-25 01:55:52 -0300

    atualizações no projetos.html

--- assets/home.js
+++ assets/home.js
@@ -6,52 +6,52 @@ function scopoColor() {
         color4 = '#ed64a6'
     ]
     let numberColor = parseInt(Math.random() * colorAraay.length);
-    let afterAboutMe = document.querySelector(".about-me-tittle");
-    let afterLanguage = document.querySelector(".language-tittle")
+    let afterAboutMe = document.querySelector(".about-me-title");
+    let afterLanguage = document.querySelector(".language-title");
     
     if (colorAraay[numberColor] === '#3182ce') {
-        afterAboutMe.classList.add('about-me-tittle-blue');
-        afterAboutMe.classList.remove('about-me-tittle-purple');
-        afterAboutMe.classList.remove('about-me-tittle-green');
-        afterAboutMe.classList.remove('about-me-tittle-pink');
+        afterAboutMe.classList.add('about-me-title-blue');
+        afterAboutMe.classList.remove('about-me-title-purple');
+        afterAboutMe.classList.remove('about-me-title-green');
+        afterAboutMe.classList.remove('about-me-title-pink');
 
-        afterLanguage.classList.add('language-tittle-blue');
-        afterLanguage.classList.remove('language-tittle-purple');
-        afterLanguage.classList.remove('language-tittle-green');
-        afterLanguage.classList.remove('language-tittle-pink');
+        afterLanguage.classList.add('language-title-blue');
+        afterLanguage.classList.remove('language-title-purple');
+        afterLanguage.classList.remove('language-title-green');
+        afterLanguage.classList.remove('language-title-pink');
     }
     if (colorAraay[numberColor] === '#9f7aea') {
-        afterAboutMe.classList.remove('about-me-tittle-blue');
-        afterAboutMe.classList.add('about-me-tittle-purple');
-        afterAboutMe.classList.remove('about-me-tittle-green');
-        afterAboutMe.classList.remove('about-me-tittle-pink');
+        afterAboutMe.classList.remove('about-me-title-blue');
+        afterAboutMe.classList.add('about-me-title-purple');
+        afterAboutMe.classList.remove('about-me-title-green');
+        afterAboutMe.classList.remove('about-me-title-pink');
 
-        afterLanguage.classList.remove('language-tittle-blue');
-        afterLanguage.classList.add('language-tittle-purple');
-        afterLanguage.classList.remove('language-tittle-green');
-        afterLanguage.classList.remove('language-tittle-pink');
+        afterLanguage.classList.remove('language-title-blue');
+        afterLanguage.classList.add('language-title-purple');
+        afterLanguage.classList.remove('language-title-green');
+        afterLanguage.classList.remove('language-title-pink');
     }
     if (colorAraay[numberColor] === '#48bb78') {
-        afterAboutMe.classList.remove('about-me-tittle-blue');
-        afterAboutMe.classList.remove('about-me-tittle-purple');
-        afterAboutMe.classList.add('about-me-tittle-green');
-        afterAboutMe.classList.remove('about-me-tittle-pink');
+        afterAboutMe.classList.remove('about-me-title-blue');
+        afterAboutMe.classList.remove('about-me-title-purple');
+        afterAboutMe.classList.add('about-me-title-green');
+        afterAboutMe.classList.remove('about-me-title-pink');
 
-        afterLanguage.classList.remove('language-tittle-blue');
-        afterLanguage.classList.remove('language-tittle-purple');
-        afterLanguage.classList.add('language-tittle-green');
-        afterLanguage.classList.remove('language-tittle-pink');
+        afterLanguage.classList.remove('language-title-blue');
+        afterLanguage.classList.remove('language-title-purple');
+        afterLanguage.classList.add('language-title-green');
+        afterLanguage.classList.remove('language-title-pink');
     }
     if (colorAraay[numberColor] === '#ed64a6') {
-        afterAboutMe.classList.remove('about-me-tittle-blue');
-        afterAboutMe.classList.remove('about-me-tittle-purple');
-        afterAboutMe.classList.remove('about-me-tittle-green');
-        afterAboutMe.classList.add('about-me-tittle-pink');
+        afterAboutMe.classList.remove('about-me-title-blue');
+        afterAboutMe.classList.remove('about-me-title-purple');
+        afterAboutMe.classList.remove('about-me-title-green');
+        afterAboutMe.classList.add('about-me-title-pink');
 
-        afterLanguage.classList.remove('language-tittle-blue');
-        afterLanguage.classList.remove('language-tittle-purple');
-        afterLanguage.classList.remove('language-tittle-green');
-        afterLanguage.classList.add('language-tittle-pink');
+        afterLanguage.classList.remove('language-title-blue');
+        afterLanguage.classList.remove('language-title-purple');
+        afterLanguage.classList.remove('language-title-green');
+        afterLanguage.classList.add('language-title-pink');
     }
 
 
@@ -99,13 +99,11 @@ function scopoHamburguer() {
 
     buttonHamburguer.onclick = function Open() {
         if (inputHamburguer.checked === false) {
-            console.log('passou aqaaaaaaaa')
             inputHamburguer.checked = true;
             buttonHamburguer.innerHTML = `Close <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1.5rem" width="1.5rem" xmlns="http://www.w3.org/2000/svg"><path d="M2.165,19.551C2.351,19.831,2.664,20,3,20h15c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944 C21.649,11.169,21.336,11,21,11h-1V7c0-1.103-0.897-2-2-2h-6.1L9.616,3.213C9.44,3.075,9.224,3,9,3H4C2.897,3,2,3.897,2,5v14 h0.007C2.007,19.192,2.056,19.385,2.165,19.551z M17.341,18H4.517l2.143-5h12.824L17.341,18z M18,7v4H6 c-0.4,0-0.762,0.238-0.919,0.606L4,14.129V7h7.556H12H18z"></path></svg>`;
             close();
         }
         else if (inputHamburguer.checked === true) {
-            console.log('passou aqqqqq')
             inputHamburguer.checked = false;
             buttonHamburguer.innerHTML = `Open
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"height="1.5rem" width="1.5rem" xmlns="http://www.w3.org/2000/svg"> <path d="M20,5h-8.586L9.707,3.293C9.52,3.105,9.265,3,9,3H4C2.897,3,2,3.897,2,5v14c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2V7 C22,5.897,21.103,5,20,5z M4,19V7h7h1h8l0.002,12H4z"> </path> </svg>`;
